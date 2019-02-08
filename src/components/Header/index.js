82e6b874c9ecea3e2572455f8499b6e6a92c97d3
lekan <kokulekan23@gmail.com>
2019-02-08 12:19:22 +0100

    fixed bug

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,6 +18,7 @@ class Header extends Component {
     this.setState({ dropdownOpen: false });
   };
   render() {
+    let { searchOpen } = this.state;
     let { auth } = this.props;
     console.log(JSON.stringify(auth.user));
 
